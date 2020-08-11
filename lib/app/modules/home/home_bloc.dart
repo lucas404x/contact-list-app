2512b6f97848599ab95c7cf0549804572e4b6aaf
José Lucas da Silva <joselucas0303@gmail.com>
2020-08-11 15:58:15 -0300

    added i/o streams

--- lib/app/modules/home/home_bloc.dart
+++ lib/app/modules/home/home_bloc.dart
@@ -1,9 +1,39 @@
+import 'dart:async';
+
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'models/contact_model.dart';
+import 'services/local_database_service.dart';
 
 class HomeBloc extends BlocBase {
-  //dispose will be called automatically by closing its streams
+  LocalDatabaseService _localDatabaseService;
+  List<ContactModel> _contacts;
+
+  HomeBloc(this._localDatabaseService) {
+    _init();
+  }
+
+  final _contactData = BehaviorSubject<List<ContactModel>>.seeded([]);
+  final _contactObserver = StreamController();
+
+  Stream get contactDataOut => _contactData.stream;
+  Sink get contactDataInp => _contactObserver.sink;
+
+  _init() {
+    _contactObserver.stream.listen(_updateContactList);
+    _updateContactList(null);
+  }
+
+  Future<void> _updateContactList(_) async {
+    _contacts = await _localDatabaseService.getAllContactsOrderByAZ();
+    _contactData.sink.add(_contacts);
+  }
+
   @override
   void dispose() {
     super.dispose();
+    _contactData.close();
+    _contactObserver.close();
   }
 }
