601632e296362cae65498f258adf93b8ee82ab17
José Lucas da Silva <joselucas0303@gmail.com>
2020-08-11 15:57:18 -0300

    added dependencies and the home_bloc on blocs list

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,16 +1,24 @@
-import 'package:contact_list/app/modules/home/home_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:contact_list/app/modules/home/home_page.dart';
+
+import 'data/local_database.dart';
+import 'home_bloc.dart';
+import 'home_page.dart';
+import 'services/local_database_service.dart';
 
 class HomeModule extends ModuleWidget {
   @override
   List<Bloc> get blocs => [
-        Bloc((i) => HomeBloc()),
+        Bloc(
+          (i) => HomeBloc(i.get<LocalDatabaseService>()),
+        )
       ];
 
   @override
-  List<Dependency> get dependencies => [];
+  List<Dependency> get dependencies => [
+        Dependency((i) => LocalDatabaseService(LocalDatabase()),
+            singleton: true)
+      ];
 
   @override
   Widget get view => HomePage();
